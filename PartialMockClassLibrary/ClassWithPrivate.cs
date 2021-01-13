b796ace39788d2019f2bca5659eb0cb2c88bc098
Agatino Pesce <a.pesce@mecal.com>
2021-01-13 08:04:19 +0100

    PrivateObject for both Property and Method

---
+++ PartialMockClassLibrary/ClassWithPrivate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialMockClassLibrary
+{
+    public class ClassWithPrivate
+    {
+        protected int _numero { get; set; }
+
+
+        public int Numero => _numero;
+        
+        public ClassWithPrivate(int numero)
+        {
+            _numero = numero;
+        }
+
+        public string DoppiaNumero()
+        {
+            _doppiaNumero();
+            return Numero.ToString();
+        }
+
+        private void _doppiaNumero()
+        {
+            _numero=_numero*2;
+        }
+    }
+}
---
+++ PartialMockClassLibraryTest/ClassWithPrivateTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PartialMockClassLibrary;
+
+namespace PartialMockClassLibraryTest
+{
+    [TestClass]
+    public class ClassWithPrivateTest
+    {
+        [TestMethod]
+        public void TestPrivate()
+        {
+            ClassWithPrivate classWithPrivate = new ClassWithPrivate(3);
+
+//            var privateType = new PrivateType(typeof(ClassWithPrivate));
+            PrivateObject po1 = new PrivateObject(classWithPrivate);
+            int pre1 = (int)po1.GetProperty("_numero");
+            int pre11=(int)(new PrivateObject(classWithPrivate).GetProperty("_numero"));
+            po1.Invoke("_doppiaNumero");
+            int post = (int)po1.GetFieldOrProperty("_numero");
+            Assert.AreEqual(post, 6);
+        }
+
+    }
+}
